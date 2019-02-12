e810fd034ec8c97b8af4992d229fd0a030b6a6bb
Pedro Santos <pedro.santos@moxy.studio>
2019-02-12 15:11:07 +0000

    feat: add buttons (primary, secondary, tertiary)

---
+++ src/components/button/Button.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import styles from './Button.css';
+
+export default class Button extends Component {
+    static propTypes = {
+        variant: PropTypes.oneOf(['primary', 'secondary', 'tertiary']).isRequired,
+        disabled: PropTypes.bool,
+        fullWidth: PropTypes.bool,
+        children: PropTypes.node.isRequired,
+    };
+
+    render() {
+        const { variant, disabled, children, fullWidth, ...rest } = this.props;
+        const finalClassName = classNames(
+            styles.button,
+            styles[variant],
+            fullWidth && styles.fullWidth,
+        );
+
+        return (
+            <button { ...rest } disabled={ disabled } className={ finalClassName } >
+                <span>{ children }</span>
+            </button>
+        );
+    }
+}
---
+++ src/components/button/README.md
@@ -0,0 +1,21 @@
+# Button
+
+A standard button.
+
+## Usage
+
+```jsx
+import { Button } from '@idm/web-uikit';
+
+<Button variant="primary" onClick={ handleClick } />
+```
+
+## Props
+| name      | type    | default    | description                                                                    |
+|-----------|---------|------------|--------------------------------------------------------------------------------|
+| variant   | string  | *required* | Sets the button variant. Can be one of: `primary`, `secondary` or `tertiary`   |
+| disabled  | boolean | false      | Sets the disable state                                                         |
+| fullWidth | boolean | false      | Sets button width to 100%                                                      |
+
+
+**Note:** Any other properties supplied to this component will be spread to the root element.
