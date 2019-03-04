61f2724402c7185e71f467322bca92e0eda66370
Gil Domingues <gil.domingues2@hotmail.com>
2019-03-04 18:18:22 +0000

    chore: improved slider related code

--- src/components/slider/display/Display.js
+++ src/components/slider/display/Display.js
@@ -11,31 +11,27 @@ export default class Display extends Component {
 
     render() {
         const { currentValue } = this.props;
-        const displayWrapper = classNames(styles['display-wrapper']);
-        const counterWrapper = classNames(styles['counter-wrapper']);
-        const digitsDisplay = classNames(styles['digits-display']);
         const ten = classNames(styles.ten, styles['digit-wrapper']);
         const one = classNames(styles.one, styles['digit-wrapper']);
-        const minText = classNames(styles.minutes);
 
         const tenDigit = Math.floor(currentValue / 10);
         const tens = [0, 1];
         const ones = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
         return (
-            <div className={ displayWrapper }>
-                <div className={ counterWrapper }>
-                    <div className={ digitsDisplay }>
+            <div className={ styles['display-wrapper'] }>
+                <div className={ styles['counter-wrapper'] }>
+                    <div className={ styles['digits-display'] }>
                         <div className={ ten } style={ { transform: `translateY(${-9 * (tenDigit)}rem)` } }>
                             {tens.map((elem) => <div key={ elem } >{ elem }</div>)}
                         </div>
                     </div>
-                    <div className={ digitsDisplay }>
+                    <div className={ styles['digits-display'] }>
                         <div className={ one } style={ { transform: `translateY(${-9 * (currentValue - 1)}rem)` } }>
                             {ones.map((elem) => <div key={ elem } >{ elem }</div>)}
                         </div>
                     </div>
-                    <h3 className={ minText }>
+                    <h3 className={ styles.minutes }>
                         min
                     </h3>
                 </div>
--- stories/slider.js
+++ stories/slider.js
@@ -10,5 +10,7 @@ storiesOf('Slider', module)
 .addDecorator(withReadme(readme))
 .addDecorator(withKnobs)
 .add('Slider', () => (
-    <Slider defaultValue={ 3 } handleSliderChange={ action('clicked!') } />
+    <div style={ { width: '47.4rem', margin: 'auto auto' } }>
+        <Slider defaultValue={ 3 } handleSliderChange={ action('clicked!') } />
+    </div>
 ));
