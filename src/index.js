4bcf24ebed1d5ba643998082f97e1f6639bee4e9
André Cruz <andremiguelcruz@msn.com>
2019-04-16 10:52:42 +0100

    feat: add react-keyboard-only-outlines

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export { default as KeyboardOnlyOutlines } from 'react-keyboard-only-outlines';
 export { default as Button } from './components/button';
 export { default as TextInput } from './components/text-input';
 export { default as Icon } from './components/icon';
