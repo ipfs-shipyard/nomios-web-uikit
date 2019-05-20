6eaea4c993ec6a7543aa8883e89b4fef7f2630aa
Pedro Santos <pedro.santos@moxy.studio>
2019-05-20 22:53:54 +0100

    chore: update AutocompleteSelect to get value from options object

--- src/components/autocomplete-select/AutocompleteSelect.js
+++ src/components/autocomplete-select/AutocompleteSelect.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Select from 'react-select';
-
 import styles from './AutocompleteSelect.css';
 
 const hideMenuWithNoOptions = () => 'No options available';
 
-const AutocompleteSelect = ({ className, ...rest }) => (
-    <Select
-        { ...rest }
-        className={ classNames(styles.container, className) }
-        classNamePrefix="autocomplete" />
-);
+class AutocompleteSelect extends Component {
+    render() {
+        const { className, ...rest } = this.props;
+
+        return (
+            <Select
+                { ...rest }
+                onChange={ this.handleSelectChange }
+                value={ this.getValueFromOptions() }
+                className={ classNames(styles.container, className) }
+                classNamePrefix="autocomplete" />
+        );
+    }
+
+    getValueFromOptions() {
+        return this.props.options.find((option) => option.value === this.props.value);
+    }
+
+    handleSelectChange = (option) => this.props.onChange(option.value);
+}
 
 AutocompleteSelect.propTypes = {
+    options: PropTypes.array.isRequired,
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired,
     noOptionsMessage: PropTypes.func,
     className: PropTypes.string,
 };
