89bc5fc67e1fe22007aec3504064f5250f108811
André Cruz <andremiguelcruz@msn.com>
2019-05-21 12:42:05 +0100

    fix: fix missing forwardRef in icon

--- src/components/icon/Icon.js
+++ src/components/icon/Icon.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Svg from '../svg';
 import styles from './Icon.css';
 
-const Icon = (props) => {
+const Icon = forwardRef((props, ref) => {
     const { className, ...rest } = props;
     const finalProps = {
         ...rest,
         className: classNames(styles.icon, className),
     };
 
-    return <Svg { ...finalProps } />;
-};
+    return <Svg ref={ ref } { ...finalProps } />;
+});
 
 Icon.propTypes = {
     svg: PropTypes.oneOfType([PropTypes.string, PropTypes.shape({ then: PropTypes.func.isRequired })]).isRequired,
